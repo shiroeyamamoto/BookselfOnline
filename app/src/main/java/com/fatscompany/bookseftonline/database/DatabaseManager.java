ad5887bccecc0d387cc603ba3435b0e6353fe586
Khang Duong <khangvngm@gmail.com>
2023-07-30 15:56:44 +0700

    _Khang_DatabaseSystem@DatabaseController_and_DatabaseManager

---
+++ app/src/main/java/com/fatscompany/bookseftonline/database/DatabaseManager.java
@@ -0,0 +1,23 @@
+package com.fatscompany.bookseftonline.database;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DatabaseManager {
+    private DatabaseController dbHelper;
+    private SQLiteDatabase database;
+
+    public DatabaseManager(Context context) {
+        dbHelper = new DatabaseController(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+}
