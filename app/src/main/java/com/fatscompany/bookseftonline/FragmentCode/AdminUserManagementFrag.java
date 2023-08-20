bdb48362a92448d7c85f14b059e4320828f6fd76
CliDang <tsmarch7th@gmail.com>
2023-08-20 14:29:25 +0700

    rcv

--- app/src/main/java/com/fatscompany/bookseftonline/FragmentCode/AdminUserManagementFrag.java
+++ app/src/main/java/com/fatscompany/bookseftonline/FragmentCode/AdminUserManagementFrag.java
@@ -4,19 +4,31 @@ import android.os.Bundle;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.fatscompany.bookseftonline.AppDatabase;
 import com.fatscompany.bookseftonline.Database.DatabaseAdapter;
+import com.fatscompany.bookseftonline.Entitis.User;
 import com.fatscompany.bookseftonline.MainActivity;
 import com.fatscompany.bookseftonline.R;
 import com.fatscompany.bookseftonline.Utils.SignUp;
 import com.fatscompany.bookseftonline.databinding.AddUserBinding;
 import com.fatscompany.bookseftonline.databinding.FragmentAdminStatisticBinding;
 import com.fatscompany.bookseftonline.databinding.FragmentAdminUserManagementBinding;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import Adapter.BookAdapter;
+import Adapter.CategoryAdapter;
+import Adapter.UserAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +41,10 @@ public class AdminUserManagementFrag extends Fragment {
     private AddUserBinding addUserBinding;
     private DatabaseAdapter dbAdapter;
 
+    private List<User> userList;
+
+    private UserAdapter userAdapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,9 +54,40 @@ public class AdminUserManagementFrag extends Fragment {
         binding = FragmentAdminUserManagementBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        binding.btnCreateUser.setOnClickListener(v->{
+        binding.btnCreateUser.setOnClickListener(v -> {
             showAddUserContent();
         });
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        RecyclerView rcv = view.findViewById(R.id.userRecyclerView);
+        rcv.setLayoutManager(linearLayoutManager);
+
+        Executor executor = Executors.newSingleThreadExecutor();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                AppDatabase database = AppDatabase.getInstance(getActivity());
+                database.runInTransaction(new Runnable() {
+                    @Override
+                    public void run() {
+                        userList = database.userDao().selectAll();
+
+                        if (userList != null) {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    userAdapter = new UserAdapter(userList, getContext());
+
+                                    rcv.setAdapter(userAdapter);
+
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+
         return view;
     }
 
@@ -52,32 +99,41 @@ public class AdminUserManagementFrag extends Fragment {
         // Add the inflated layout to a container in the fragment's layout``
         binding.countainerFrameLayout.addView(addUserView);
 
-        addUserBinding.btnSave.setOnClickListener(v->{
+        addUserBinding.btnSave.setOnClickListener(v -> {
             btnsaveAddUserClick();
         });
     }
 
-    private void btnsaveAddUserClick(){
+    private void btnsaveAddUserClick() {
         String uName = addUserBinding.txtuName.getText().toString();
-        String pw  = addUserBinding.txtpassword.getText().toString();
+        String pw = addUserBinding.txtpassword.getText().toString();
         String role = addUserBinding.txtRole.getText().toString();
-        String fName  = addUserBinding.txtfName.getText().toString();
+        String fName = addUserBinding.txtfName.getText().toString();
         String lName = addUserBinding.txtfName.getText().toString();
-        String mail  = addUserBinding.txtMail.getText().toString();
+        String mail = addUserBinding.txtMail.getText().toString();
         String sdt = addUserBinding.txtSDT.getText().toString();
         Boolean active = true;
-        if (uName.isEmpty() || pw.isEmpty()){
+        if (uName.isEmpty() || pw.isEmpty()) {
             Toast.makeText(requireContext(), "Please fill out the fields", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            try {
-                dbAdapter = new DatabaseAdapter(requireContext());
-                dbAdapter.InsertUser(uName, pw, fName, lName, mail, sdt, active, role);
-                Toast.makeText(requireContext(), "Added successfully", Toast.LENGTH_SHORT).show();
+        } else {
+            AppDatabase db = AppDatabase.getInstance(requireContext());
+            Executor executor = Executors.newSingleThreadExecutor();
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
 
-            }catch (Exception ex){
-                Toast.makeText(requireContext(), "Unknown errors", Toast.LENGTH_SHORT).show();
-            }
+                    db.runInTransaction(new Runnable() {
+                        @Override
+                        public void run() {
+                            User user = new User(uName, pw, fName, lName, mail, sdt, active, role);
+
+                            db.userDao().insert(user);
+                        }
+                    });
+                }
+            });
         }
     }
+
+
 }
